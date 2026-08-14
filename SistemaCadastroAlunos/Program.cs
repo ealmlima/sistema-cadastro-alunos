93912f4a03b6793e0507ea556ae9a833c573d827
Erika Lima <eralmlima@outlook.com>
2026-08-13 23:14:13 -0300

    feat: implementa método de listagem de alunos com calculo da média

--- SistemaCadastroAlunos/Program.cs
+++ SistemaCadastroAlunos/Program.cs
@@ -1,6 +1,24 @@
 ﻿class Program
 {
-    
+    static void Main()
+    {
+
+        
+
+    }
+
+    static void ListarAlunos(string[] nomeAluno, int[] idadeAluno, double[] nota1, double[] nota2)
+    {
+        for (int i = 0; i < nomeAluno.Length; i++)
+        {
+            double mediaNotas = CalcularMedia(nota1[i], nota2[i]);
+
+            Console.WriteLine($"Nome: {nomeAluno[i]}");
+            Console.WriteLine($"Idade: {idadeAluno[i]}");
+            Console.WriteLine($"Média: {mediaNotas:0.##}");
+            Console.WriteLine("-------------------------");
+        }
+
     }
 
     static double CalcularMedia(double nota1, double nota2)
