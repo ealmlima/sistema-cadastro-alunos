b9d887c1644020d80bd230627462d25051c95ccb
Erika Lima <eralmlima@outlook.com>
2026-08-13 23:08:09 -0300

    feat:adiciona método para calcular a média das notas

--- SistemaCadastroAlunos/Program.cs
+++ SistemaCadastroAlunos/Program.cs
@@ -1,7 +1,10 @@
 ﻿class Program
 {
-    static void Main()
+    
+    }
+
+    static double CalcularMedia(double nota1, double nota2)
     {
-        // ...
+        return (nota1 + nota2) / 2;
     }
 }
