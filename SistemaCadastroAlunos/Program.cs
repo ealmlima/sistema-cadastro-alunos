bb78782e68c3a895dfc1732cb8eefeac8d4b3398
Erika Lima <eralmlima@outlook.com>
2026-08-14 22:53:08 -0300

    style: ajusta formatação da média com uma casa decimal

--- SistemaCadastroAlunos/Program.cs
+++ SistemaCadastroAlunos/Program.cs
@@ -2,8 +2,64 @@
 {
     static void Main()
     {
+        string[] nomeAluno =
+        {
+            "João",
+            "Maria",
+            "Pedro",
+            "Ana",
+            "Carlos",
+            "Juliana",
+            "Lucas",
+            "Beatriz",
+            "Rafael",
+            "Camila"
+        };
 
-        
+        int[] idadeAluno =
+        {
+            18,
+            19,
+            20,
+            18,
+            21,
+            19,
+            22,
+            20,
+            18,
+            21
+        };
+
+        double[] nota1 =
+        {
+            8.5,
+            7.0,
+            9.5,
+            6.0,
+            8.0,
+            7.5,
+            9.0,
+            6.5,
+            5.5,
+            10.0
+        };
+
+        double[] nota2 =
+        {
+            7.5,
+            8.0,
+            9.0,
+            7.5,
+            8.5,
+            9.0,
+            8.0,
+            7.5,
+            6.5,
+            9.5
+        };
+
+        ListarAlunos(nomeAluno, idadeAluno, nota1, nota2);
+
 
     }
 
@@ -15,7 +71,7 @@
 
             Console.WriteLine($"Nome: {nomeAluno[i]}");
             Console.WriteLine($"Idade: {idadeAluno[i]}");
-            Console.WriteLine($"Média: {mediaNotas:0.##}");
+            Console.WriteLine($"Média: {mediaNotas:F1}");
             Console.WriteLine("-------------------------");
         }
 
