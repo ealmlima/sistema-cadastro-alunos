ca329d319bb4c9018eca2038eec2affe2d6c9ae0
majuliah <majulialcarvalho@gmail.com>
2026-08-16 19:42:57 -0300

    Envia método de exibir alunos aprovados

--- SistemaCadastroAlunos/Program.cs
+++ SistemaCadastroAlunos/Program.cs
@@ -2,6 +2,29 @@
 {
     static void Main()
     {
-        // ...
+        static void ExibirAprovados(string[] nomeAluno, double[] nota1, double[] nota2)
+        {
+            int totalAprovados = 0;
+
+            Console.WriteLine("=== Alunos aprovados ===");
+            Console.WriteLine();
+
+            for (int i = 0; i < nomeAluno.Length; i++)
+            {
+                double media = CalcularMedia(nota1[i], nota2[i]);
+
+                if (media >= 7)
+                {
+                    Console.WriteLine($"{nomeAluno[i]} - Média: {media:F1}");
+                    totalAprovados++;
+                }
+
+                //não lê a idade do aluno porque não é exibido no resultado final
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Total: {totalAprovados} alunos");
+        }
+        ExibirAprovados(nomeAluno, nota1, nota2);
     }
 }
